9c19acc8ac194162f13a6f6f4d170036b01c49da
Florian Richter <florian.richter@sap.com>
2021-12-08 11:15:29 +0100

    Make BuPa call to CC8

--- example-1/routes/users.js
+++ example-1/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var { BusinessPartner } = require('@sap/cloud-sdk-vdm-business-partner-service');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const myDestination = { destinationName: 'CC8-HTTP-BASIC' };
+
+router.get('/', async function(req, res, next) {
+  const partners = await BusinessPartner.requestBuilder()
+    .getAll()
+    .select(BusinessPartner.FIRST_NAME, BusinessPartner.LAST_NAME)
+    .filter(BusinessPartner.FIRST_NAME.equals('Florian'))
+    .execute(myDestination);
+  res.send(partners);
 });
+
+// router.post('/', async function(req, res, next) {
+//   const businessPartner = BusinessPartner.builder()
+//     .firstName('Peter')
+//     .lastName('Pan')
+//     .toBusinessPartnerAddress([
+//       BusinessPartnerAddress.builder()
+//         .country('Neverland')
+//         .build()
+//     ])
+//     .build();
+
+//   res.send(await BusinessPartner.requestBuilder().create(businessPartner).execute(myDestination));
+// });
 
 module.exports = router;
